af9ad0353be48d8a7df80524ca74e61f78404c40
alfonso lara <orozcoenrique159@gmail.com>
2025-05-07 17:28:09 -0500

    problema en equipo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ahora es hora de trabajar");
+        //aqui hay un problema
+        
     }
 }

9ee8ab7e3bef7a4a3ac38cd21188699ec7348a4f
alfonso lara <orozcoenrique159@gmail.com>
2025-05-07 18:03:57 -0500

    feature 2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //aqui hay un problema
+        System.out.println("Esta es una nueva versión");
+        System.out.println("este es un nuevo feature");
         
     }
 }
